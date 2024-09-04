5ddeaeb8192645ff979c55a31cb8f5fa313a7ae6
opemii <opemipousuf@gmail.com>
2024-09-04 12:15:27 +0100

    fix:iam policy

--- gcp-infra/iam.tf
+++ gcp-infra/iam.tf
@@ -44,23 +44,53 @@ resource "google_storage_bucket_iam_member" "tf_apply_storage_object_admin" {
   member = "serviceAccount:${google_service_account.tf_apply.email}"
 }
 
-resource "google_project_iam_member" "tf_apply_service_account_admin" {
-  project = var.project_id
-  role    = "roles/iam.serviceAccountAdmin"
-  member  = "serviceAccount:${google_service_account.tf_plan.email}"
+# resource "google_project_iam_member" "tf_apply_service_account_admin" {
+#   project = var.project_id
+#   role    = "roles/iam.serviceAccountAdmin"
+#   member  = "serviceAccount:${google_service_account.tf_plan.email}"
+# }
+
+data "google_iam_policy" "tf_apply_policy1" {
+  binding {
+    role = "roles/iam.workloadIdentityUser"
+
+    members = [
+      "principalSet://iam.googleapis.com/projects/${data.google_project.project.number}/locations/global/workloadIdentityPools/gh-pool/attribute.full/${var.gh_repo}${var.gh_branch}",
+    ]
+  }
 }
 
-resource "google_service_account_iam_member" "tf_plan_workload_identity_user" {
-  service_account_id = "serviceAccount:${google_service_account.tf_plan.email}"
-  role               = "roles/iam.workloadIdentityUser"
-  member             = "principalSet://iam.googleapis.com/projects/${var.project_number}/locations/global/workloadIdentityPools/github/attribute.event_name/pull_request"
+data "google_iam_policy" "tf_apply_policy2" {
+  binding {
+    role = "roles/iam.serviceAccountAdmin"
+
+    members = [
+      "serviceAccount:${google_service_account.tf_plan.email}",
+    ]
+  }
 }
 
-resource "google_service_account_iam_member" "tf_apply_workload_identity_user" {
-  service_account_id = "serviceAccount:${google_service_account.tf_apply.email}"
-  role               = "roles/iam.workloadIdentityUser"
-  member             = "principalSet://iam.googleapis.com/projects/${var.project_number}/locations/global/workloadIdentityPools/github-pool/attribute.workflow_ref/opemmii/workload-identity-federation/.github/workflows/terraform.yaml@refs/heads/main"
+data "google_iam_policy" "tf_plan_policy" {
+  binding {
+    role = "roles/iam.workloadIdentityUser"
+
+    members = [
+      "principalSet://iam.googleapis.com/projects/${var.project_number}/locations/global/workloadIdentityPools/github/attribute.event_name/pull_request",
+    ]
+  }
 }
+
+# resource "google_service_account_iam_member" "tf_plan_workload_identity_user" {
+#   service_account_id = google_service_account.tf_plan.email
+#   role               = "roles/iam.workloadIdentityUser"
+#   member             = "principalSet://iam.googleapis.com/projects/${var.project_number}/locations/global/workloadIdentityPools/github/attribute.event_name/pull_request"
+# }
+
+# resource "google_service_account_iam_member" "tf_apply_workload_identity_user" {
+#   service_account_id = google_service_account.tf_apply.email
+#   role               = "roles/iam.workloadIdentityUser"
+#   member             = "principalSet://iam.googleapis.com/projects/${var.project_number}/locations/global/workloadIdentityPools/github-pool/attribute.workflow_ref/opemmii/workload-identity-federation/.github/workflows/terraform.yaml@refs/heads/main"
+# }
 
 
 
@@ -87,8 +117,7 @@ resource "google_iam_workload_identity_pool_provider" "github-provider" {
     "attribute.repository_owner"= "assertion.repository_owner"
     "attribute.workflow_ref" = "assertion.job_workflow_ref"
     "attribute.event_name"= "assertion.event_name"
-  attribute-condition="assertion.repository_owner == 'opemii/assessment'" 
-
+    "attribute.full" = "assertion.repository+assertion.ref"
   }
   oidc {
     allowed_audiences = ["google-wlif"]
